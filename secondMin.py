a36753a2662961cc1171fcd8a4b4bd6fae7792d1
HiddenPeufien <90607290+HiddenPeufien@users.noreply.github.com>
2022-02-10 12:38:16 +0100

    Create secondMin.py

---
+++ secondMin.py
@@ -0,0 +1,16 @@
+def secondMin(numbers):
+
+    # initialisation
+    m1 = m2 = float('inf')
+
+    # algorithm
+    for x in numbers:
+        if x <= m1:
+            m1, m2 = x, m1
+        elif x < m2:
+            m2 = x
+
+    # return        
+    if m2 == float('inf') or m2 == m1:
+        m2 = None
+    return m2
