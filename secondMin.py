7cd01a2846050ab2c0916fddfbcd9bf339286c51
HiddenPeufien <90607290+HiddenPeufien@users.noreply.github.com>
2022-04-14 15:34:51 +0200

    test should fail

--- secondMin.py
+++ secondMin.py
@@ -8,7 +8,8 @@ def secondMin(numbers):
         if x <= m1:
             m1, m2 = x, m1
         elif x < m2:
-            m2 = x
+            m2 = x +1
+            
 
     # return        
     if m2 == float('inf') or m2 == m1:
